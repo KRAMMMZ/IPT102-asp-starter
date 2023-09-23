7303df29545778a4af8d3a6a478a6a0f317cd30e
Mark Gervic Arca <markgervicarca@gmail.com>
2023-09-23 15:06:36 +0800

    Updated student list

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,9 +6,9 @@ public class HomeController : Controller
     {
         var studentInfoArray = new[]
         {
-            new StudentInfoModel { Name = "John Doe", StudentId = "S001", StudentAge = "20" },
-            new StudentInfoModel { Name = "Jane Smith", StudentId = "S002", StudentAge = "22" },
-            new StudentInfoModel { Name = "Mike Johnson", StudentId = "S003", StudentAge = "31" }
+            new StudentInfoModel { Name = "Mark Gervic Arca", StudentId = "S001", StudentAge = "20" },
+            new StudentInfoModel { Name = "Roronoa Zoro", StudentId = "S002", StudentAge = "22" },
+            new StudentInfoModel { Name = "Uchiha Itachi", StudentId = "S003", StudentAge = "31" }
         };
 
         return View(studentInfoArray);
